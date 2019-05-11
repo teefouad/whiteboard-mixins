cc14719ed8a80ddd3fa8cccf717d6702ca52e15f
Mostafa Fouad <mostafa.fouad@incorta.com>
2019-05-11 14:38:30 +0200

    Updates

---
+++ precache-manifest.c66e880d7670d46e28d6fc46158adc4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104cbd706031d680fb1632d56d9bf9f7",
+    "url": "/makinah-demo/index.html"
+  },
+  {
+    "revision": "8eeb1b31cce82be15f1a",
+    "url": "/makinah-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/makinah-demo/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "8eeb1b31cce82be15f1a",
+    "url": "/makinah-demo/static/js/main.1328dbf3.chunk.js"
+  },
+  {
+    "revision": "6d417453e50a17d36fc3",
+    "url": "/makinah-demo/static/js/runtime~main.5c0c03c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/makinah-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
